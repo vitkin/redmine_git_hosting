8bf2a72c437a60c185919db8ac33af054e218e8b
Victor Itkin <victor.itkin@gmail.com>
2013-03-27 16:01:49 +0200

    WIP - Changes for compatibility with Redmine 2.3.0: Replacing ZeroClipboard v1.0.7 with v1.1.7 Still need to complete the move by correcting the files _git_urls.erb, zero_clipboard_setup.js and git_url_display.css to remove what is not needed anymore and add a tooltip when the mouse is over the clipboard button.

--- assets/javascripts/zero_clipboard_setup.js
+++ assets/javascripts/zero_clipboard_setup.js
@@ -1,38 +1,87 @@
 var zero_clipboard_source_input_control_id = "git_url_text";
-var clipboard = null
+var clipboard = null;
+
+// jquery stuff (optional)
+function debugstr(text)
+{
+  $("#d_debug").append($("<p>").text(text));
+}
 
 function reset_zero_clipboard()
 {
-	var clip_container = $('clipboard_container');
-	if (clip_container) {
-		clip_container.show();
-		clip_container.style.fontFamily="serif"
+  var clip_container = document.getElementById("clipboard_container");
 
-		var cur_children = clip_container.childNodes;
-		var ci;
-		for(ci=0; ci< cur_children.length; ci++)
-		{
-			var c = cur_children[ci];
-			if(c.id != "clipboard_button")
-			{
-				clip_container.removeChild(c);
-			}
-		}
+  if (clip_container)
+  {
+    clip_container.style.display = "";
+    clip_container.style.fontFamily = "serif";
 
-		clipboard = new ZeroClipboard.Client();
+    var cur_children = clip_container.childNodes;
+    var ci;
 
-		clipboard.setHandCursor(true);
-		clipboard.glue('clipboard_button', 'clipboard_container');
+    for (ci = 0; ci < cur_children.length; ci++)
+    {
+      var c = cur_children[ci];
 
-		clipboard.addEventListener('mouseOver', function (client) {
-			clipboard.setText($(zero_clipboard_source_input_control_id).value);
-		});
-	}
+      if (c.id != "clipboard_button")
+      {
+        clip_container.removeChild(c);
+      }
+    }
+
+    clipboard = new ZeroClipboard(document.getElementById("clipboard_button"));
+    //clipboard = new ZeroClipboard.Client();
+    
+    //clipboard.setHandCursor(true);
+    //clipboard.glue(document.getElementById("clipboard_container"));
+    //clipboard.glue('clipboard_button', 'clipboard_container');
+    
+    //debugstr("truc");
+
+    
+    clipboard.on('mouseOver', function(client)
+    {
+      //debugstr("Value " + document.getElementById(zero_clipboard_source_input_control_id).value);
+      clipboard.setText(document.getElementById(zero_clipboard_source_input_control_id).value);
+      document.getElementById("clipboard_button").style.background = "#507AAA";
+    });
+    
+    clipboard.on('mouseOut', function(client)
+    {
+      document.getElementById("clipboard_button").style.background = "#eee";
+    });
+
+    clipboard.on('load', function(client)
+    {
+      //debugstr("Flash movie loaded and ready.");
+    });
+
+    clipboard.on('complete', function(client, args)
+    {
+      //debugstr("Copied text to clipboard: " + args.text);
+      document.getElementById("clipboard_button").style.background = "#bbb";
+      
+      setTimeout(function () 
+      {
+        document.getElementById("clipboard_button").style.background = "#eee";
+      }, 250);
+      
+    });
+    
+    
+    /*clipboard.addEventListener('mouseOver', function (client) {
+      debugstr("Value " + document.getElementById(zero_clipboard_source_input_control_id).value);
+			clipboard.setText(document.getElementById(zero_clipboard_source_input_control_id).value);
+		});*/
+  }
 }
 
 function setZeroClipboardInputSource(id)
 {
-	zero_clipboard_source_input_control_id = id;
+  zero_clipboard_source_input_control_id = id;
 }
 
-document.observe("dom:loaded", function() { reset_zero_clipboard(); } )
+$(document).ready(function()
+{
+  reset_zero_clipboard();
+});
